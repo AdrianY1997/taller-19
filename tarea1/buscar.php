599bd2a811235ade4527698fc9a6b14553fb5113
BrayanABP <parrabarriosbbb@gmail.com>
2023-05-09 07:27:05 -0500

    Avance 1

--- tarea1/buscar.php
+++ tarea1/buscar.php
@@ -1,8 +1,10 @@
 <html>
+
 <head>
     <title>conecta</title>
     <link rel="stylesheet" href="./tabla.css">
 </head>
+
 <body>
     <html>
 
@@ -11,30 +13,29 @@
     </head>
 
     <body>
-        <br>
         <div class="tabla">
-        <table class="tab">
-            <tr>
-                <td class="tab">codigo</td>
-                <td class="tab">nombre </td>
-                <td class="tab">descripcion </td>
-            </tr>
-            </div>
-            <?php
-            include("conexion.php");
-            $enlace = conectar();
-            $sql = "SELECT * from productos";
-            $result = mysqli_query($enlace, $sql);
-            while ($mostrar = mysqli_fetch_array($result)) {
-            ?>
-                <tr>
-                    <td><?php echo "" . $mostrar['idproducto']; ?></td>
-                    <td><?php echo "" . $mostrar['nombre']; ?></td>
-                    <td><?php echo "" . $mostrar['descripcion']; ?></td>
+            <table class="camp">
+                <tr class="tab">
+                    <td class="tab">codigo</td>
+                    <td class="tab">nombre </td>
+                    <td class="tab">descripcion </td>
                 </tr>
-            <?php
-            }
-            ?>
+        </div>
+        <?php
+        include("conexion.php");
+        $enlace = conectar();
+        $sql = "SELECT * from productos";
+        $result = mysqli_query($enlace, $sql);
+        while ($mostrar = mysqli_fetch_array($result)) {
+        ?>
+            <tr>
+                <td><?php echo "" . $mostrar['idproducto']; ?></td>
+                <td><?php echo "" . $mostrar['nombre']; ?></td>
+                <td><?php echo "" . $mostrar['descripcion']; ?></td>
+            </tr>
+        <?php
+        }
+        ?>
         </table>
     </body>
 
