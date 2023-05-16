ed3411188de9643ee27c772de33f45c5e6823efe
GUANKI <jm065726@gmail.com>
2023-05-16 08:15:34 -0500

    Taller 1 y 2

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+ //Se incluye la configuración de conexión a datos en el
+ //SGBD: MariaDB.
+ require_once 'model/database.php';
+ //Para registrar productos es necesario iniciar los proveedores
+ //de los mismos, por ello la variable controller para este
+ //ejercicio se inicia con el ‘proveedor’.
+ $controller = 'proveedor';
+ // Todo esta lógica hará el papel de un FrontController
+ if(!isset($_REQUEST['c']))
+ {
+ //Llamado de la página principal
+ require_once "controller/$controller.controller.php";
+ $controller = ucwords($controller) . 'Controller';
+ $controller = new $controller;
+ $controller->Index();
+ }
+ else
+ {
+ // Obtiene el controlador a cargar
+ $controller = strtolower($_REQUEST['c']);
+ $accion = isset($_REQUEST['a']) ? $_REQUEST['a'] : 'Index';
+ // Instancia el controlador
+ require_once "controller/$controller.controller.php";
+ $controller = ucwords($controller) . 'Controller';
+ $controller = new $controller;
+ // Llama la acción
+ call_user_func( array( $controller, $accion ) );
+ }
---
+++ model/database.php
@@ -0,0 +1,35 @@
+<?php
+class Database{
+
+    private $dbname = "mvcdb";  
+    private $username = "root";
+    private $hostname = "localhost";
+    private $charset = "utf8mb4";
+    private $password = "";
+    private $pdo = null;
+    
+    function conectar (){
+        try{
+            $conexion = "mysql:host=".$this->hostname.";dbname=".$this->dbname.";charset=".$this->charset;
+            $options =[
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                PDO::ATTR_EMULATE_PREPARES => false
+            ];
+
+            // variable que contiene la conexion
+            $this-> pdo = new PDO ($conexion, $this->username, $this->password, $options);
+            return $this-> pdo;
+        }
+
+        catch(PDOException $e){
+            echo "Error de conexion" .$e->getMessage();
+            exit;
+        }
+    }
+
+
+    function desconectar (){
+        $this->pdo = null;
+    }
+}
+ 
