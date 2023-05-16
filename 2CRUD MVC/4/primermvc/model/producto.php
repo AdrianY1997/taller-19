1983506c4e80731b08a6da655fbee63ffa38712e
arcelot27 <arctamua@gmail.com>
2023-05-16 16:59:34 -0500

    ver 1 mvc

---
+++ 2CRUD MVC/4/primermvc/model/producto.php
@@ -0,0 +1,93 @@
+<?php
+class producto{
+	//Atributo para conexión a SGBD
+	private $pdo;
+	//Atributos del objeto proveedor 
+	public $idProducto;
+	public $nit;
+	public $nomprod;
+	public $precioU;
+	public $descrip;
+	//Método de conexión a SGBD.
+ 	public function __CONSTRUCT(){
+		try{
+			$this->pdo = Database::Conectar();
+		} catch(Exception $e) {
+			die($e->getMessage());
+		}
+	}
+
+	public function Listar()
+	{
+		try {
+			$result = array();
+			$stm = $this->pdo->prepare('SELECT * FROM producto');
+			$stm->execute();
+			return $stm->fetchAll(PDO::FETCH_OBJ);
+		}
+		catch(Exception $e) {
+			die($e->getMessage());
+		}
+	}
+
+	public function Obtener($idProducto) {
+		try {
+			$stm = $this->pdo->prepare('SELECT * FROM producto WHERE idProducto = ?');
+			$stm->execute(array($idProducto));
+			return $stm->fetch(PDO::FETCH_OBJ);
+		} catch (Exception $e) {
+			die($e->getMessage());
+		}
+	}
+
+	public function Eliminar($idProducto) {
+		try {
+			$stm = $this->pdo
+			->prepare('DELETE FROM producto WHERE idProducto = ?');
+			$stm->execute(array($idProducto));
+		} catch (Exception $e) {
+			die($e->getMessage());
+		}
+	}
+
+	public function Actualizar($data) {
+		try {
+			$sql = 'UPDATE producto SET
+			nomprod = ?,
+			precioU = ?,
+			descrip = ?
+			WHERE idProducto = ?';
+			$this->pdo->prepare($sql)
+			->execute(
+				array(
+					$data->nomprod,
+					$data->precioU,
+					$data->descrip,
+					$data->idProducto
+				)
+			);
+		} catch (Exception $e) {
+			die($e->getMessage());
+		}
+	}
+
+	public function Registrar(producto $data) {
+		try {
+			$sql = 'INSERT INTO producto (idProducto,nit,nomprod,precioU,descrip)
+			VALUES (?, ?, ?, ?,?)';
+			$this->pdo->prepare($sql)
+			->execute(
+				array(
+					$data->idProducto,
+					$data->nit,
+					$data->nomprod,
+					$data->precioU,
+					$data->descrip
+				)
+			);
+		} catch (Exception $e) {
+			die($e->getMessage());
+		}
+	}
+
+}
---
+++ 2CRUD MVC/5/primermvc/view/footer.php
@@ -0,0 +1,21 @@
+<div class=”row”>
+                <div class=”col-xs-12”>
+                    <hr />
+                    <footer class=”footer-distributed”>
+                        <div class=”footer-right”>
+                            <a href=”#”><i class=”fa fa-twitter”></i></a>
+                            <a href=”#”><i class=”fa fa-github”></i></a>
+                        </div>
+                        <div class=”footer-left”>
+                            <p class=”footer-links”>
+                                <a href=”index.php”>Inicio</a>
+                            </p>
+                            <p>CRUD MVC PHP + MySQL-2023</p>
+                        </div>
+                  </footer>
+                </div>
+            </div>
+        </div>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+    </body>
+</html>
